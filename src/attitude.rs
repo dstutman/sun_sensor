1aa11bc4792a20f1c6f0abd3ff38d3a71549716b
dstutman <git@stutman.tech>
2023-03-10 20:38:08 +0100

    Fix runtime panics in attitude pipeline

--- src/attitude.rs
+++ src/attitude.rs
@@ -1,17 +1,35 @@
-use nalgebra::SMatrix;
+use libm::sqrtf;
+use nalgebra::{SMatrix, Vector2};
 
-struct AttitudeEstimator {
-    position_mapping: SMatrix<f32, 2, 7>,
+pub struct AttitudePipeline<const N: usize> {
+    array_position_mapping: SMatrix<f32, 2, N>,
 }
 
-impl AttitudeEstimator {
-    pub fn update(&self, readings: SMatrix<f32, 7, 1>) {
+impl AttitudePipeline<7> {
+    pub fn new_filled_hexagon(array_scale_factor: f32) -> Self {
+        Self {
+            array_position_mapping: array_scale_factor
+                * SMatrix::from_columns(&[
+                    Vector2::new(0.0, 0.0),
+                    Vector2::new(sqrtf(3.0) / 2.0, 1.0 / 2.0),
+                    Vector2::new(0.0, 1.0),
+                    Vector2::new(-sqrtf(3.0) / 2.0, 1.0 / 2.0),
+                    Vector2::new(-sqrtf(3.0) / 2.0, -1.0 / 2.0),
+                    Vector2::new(0.0, -1.0),
+                    Vector2::new(sqrtf(3.0) / 2.0, -1.0 / 2.0),
+                ]),
+        }
+    }
+}
+
+impl<const N: usize> AttitudePipeline<N> {
+    pub fn update(&self, readings: SMatrix<f32, N, 1>) {
         let centroid = self.compute_centroid(readings);
         unimplemented!()
     }
 
-    fn compute_centroid(&self, readings: SMatrix<f32, 7, 1>) -> SMatrix<f32, 2, 1> {
-        return self.position_mapping * readings / readings.shape().0 as f32;
+    fn compute_centroid(&self, readings: SMatrix<f32, N, 1>) -> SMatrix<f32, 2, 1> {
+        return self.array_position_mapping * readings / readings.shape().0 as f32;
     }
 
     // Computes the expected unit illumination due to an emitter centered on `centroid`
