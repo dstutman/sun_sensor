6ecffd75730c9ab16886610e3ec22b29d57960da
dstutman <git@stutman.tech>
2023-03-20 14:21:37 +0100

    Implement optical attitude estimation

--- src/attitude.rs
+++ src/attitude.rs
@@ -1,35 +1,57 @@
-use libm::sqrtf;
+use core::f32::consts::PI;
+
+use libm::{atan2f, atanf, sqrtf};
 use nalgebra::{SMatrix, Vector2};
 
-pub struct AttitudePipeline<const N: usize> {
+use crate::inverse_embedding::{resistive_divider_inverse_embedding, InverseEmbeddingTable};
+
+#[derive(Clone, Copy)]
+pub struct CurrentEstimate {
+    pub azimuth: f32,
+    pub elevation: f32,
+}
+
+pub struct AttitudePipeline<'a, const N: usize> {
+    pub current_estimate: Option<CurrentEstimate>,
+
+    inverse_embedding_table: InverseEmbeddingTable<'a, N>,
     array_position_mapping: SMatrix<f32, 2, N>,
 }
 
-impl AttitudePipeline<7> {
-    pub fn new_filled_hexagon(array_scale_factor: f32) -> Self {
+impl<'a> AttitudePipeline<'a, 7> {
+    pub fn new_filled_hexagon(
+        inverse_embedding_table: InverseEmbeddingTable<'a, 7>,
+        array_scale_factor: f32,
+    ) -> Self {
         Self {
+            inverse_embedding_table,
             array_position_mapping: array_scale_factor
                 * SMatrix::from_columns(&[
                     Vector2::new(0.0, 0.0),
-                    Vector2::new(sqrtf(3.0) / 2.0, 1.0 / 2.0),
                     Vector2::new(0.0, 1.0),
-                    Vector2::new(-sqrtf(3.0) / 2.0, 1.0 / 2.0),
-                    Vector2::new(-sqrtf(3.0) / 2.0, -1.0 / 2.0),
+                    Vector2::new(-1.0 / 2.0, sqrtf(3.0) / 2.0),
+                    Vector2::new(-1.0 / 2.0, -sqrtf(3.0) / 2.0),
                     Vector2::new(0.0, -1.0),
-                    Vector2::new(sqrtf(3.0) / 2.0, -1.0 / 2.0),
+                    Vector2::new(1.0 / 2.0, -sqrtf(3.0) / 2.0),
+                    Vector2::new(1.0 / 2.0, sqrtf(3.0) / 2.0),
                 ]),
+            current_estimate: None,
         }
     }
 }
 
-impl<const N: usize> AttitudePipeline<N> {
-    pub fn update(&self, readings: SMatrix<f32, N, 1>) {
-        let centroid = self.compute_centroid(readings);
-        unimplemented!()
+impl<'a, const N: usize> AttitudePipeline<'a, N> {
+    pub fn update(&mut self, samples: SMatrix<f32, N, 1>) {
+        let readings = self.inverse_embedding_table.invert_embedding(samples);
+        let centroid = self.compute_centroid(samples);
+        self.current_estimate = Some(CurrentEstimate {
+            azimuth: atan2f(centroid[(1, 0)], centroid[(0, 0)]),
+            elevation: PI / 2.0 - atanf(13.5 * sqrtf(3.0) / 2.0 / centroid.norm()),
+        });
     }
 
     fn compute_centroid(&self, readings: SMatrix<f32, N, 1>) -> SMatrix<f32, 2, 1> {
-        return self.array_position_mapping * readings / readings.shape().0 as f32;
+        return self.array_position_mapping * readings / readings.sum();
     }
 
     // Computes the expected unit illumination due to an emitter centered on `centroid`
